5d4e95ae4490237ad7adfc7ace974b237b524c51
Nickolas <89612417+lothllann@users.noreply.github.com>
2023-01-31 18:19:27 -0300

    test

--- WEB/src/App.tsx
+++ WEB/src/App.tsx
@@ -10,8 +10,10 @@ function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        
         <Header />
         <SumaryTable />
+
       </div>
     </div>
   );
