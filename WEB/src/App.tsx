6aa42bfbcac165f734f741e9c6e530a54f128efd
Nickolas <89612417+lothllann@users.noreply.github.com>
2023-02-02 20:21:58 -0300

    notificações do browser

--- WEB/src/App.tsx
+++ WEB/src/App.tsx
@@ -2,19 +2,22 @@ import Habit from "./components/HabitDay";
 import Header from "./components/Header";
 import SumaryTable from "./components/SumaryTable";
 import "./styles/global.css";
-import './lib/dayjs'
+import "./lib/dayjs";
 
-
+window.Notification.requestPermission((permission) => {
+  if (permission === "granted") {
+    new window.Notification("Habits", {
+      body: "Texto",
+    });
+  }
+});
 
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        
         <Header />
         <SumaryTable />
-        
-
       </div>
     </div>
   );
