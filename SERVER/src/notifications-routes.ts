b28d76afb20e6dd4b6e0731dc761c140ae163335
Nickolas <89612417+lothllann@users.noreply.github.com>
2023-02-03 12:24:32 -0300

    criadas as rotas de notificação no Back-end

--- SERVER/src/notifications-routes.ts
+++ SERVER/src/notifications-routes.ts
@@ -1,11 +1,25 @@
 import { FastifyInstance } from "fastify";
-import WebPush from 'web-push'
-
-const {publicKey, privateKey} = WebPush.generateVAPIDKeys();
+import WebPush from "web-push";
 
 // const publicKey ='BG9qaoUHb0f4dNFEr-k0LfpQamUrw5GMDAXuTo9ZI-LMbaPtlLrQelAcdeDEwx4dMy3npxKSj2bjhLHOJGg4LHo'
 // const privateKey ='IeQlseBi0Q0X-_PFYoEYR5ZvMPvCoFmJITWJ_XyRwSo'
 
+const { publicKey, privateKey } = WebPush.generateVAPIDKeys();
+
+WebPush.setVapidDetails("http://localhost:3333", publicKey, privateKey);
+
 export async function notification(app: FastifyInstance) {
-  
+  app.get("/push/publick_key", () => {
+    return { publicKey };
+  });
+
+  app.post("/push/register", (request, reply) => {
+    console.log(request.body);
+    return reply.status(201).send()
+  });
+
+  app.post('/push/send', async (request, reply)=>{
+    console.log(request.body);
+    return reply.status(201).send()
+  })
 }
